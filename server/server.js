b79e1710c2f9b251e347a995c3e55cf8025d9238
Michael Roberts Jr <mike@sdcs.io>
2019-10-20 09:12:27 -0700

    adds environment var and message

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
+    let message = process.env.CUSTOM_MSG || "Hello Rio, a Node.js app is running."
     res.send('Hello Riooooo');
 });
 
