38243e871edaa0ce96bfea2701d1975c9c7fcdd5
Michael Roberts Jr <mike@sdcs.io>
2019-10-24 15:09:53 -0700

    sets a secret to pass as a key

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Riooooo');
+    let message = process.env.CUSTOM_MSG || "Hello Rio, a Node.js app is running."
+    
+    res.send(`:::: INFO :::: \n ${JSON.stringify(process.env)}\n ::::::: \n ${message}`);
 });
 
 module.exports = app;
