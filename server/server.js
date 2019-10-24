7f2730083b78be82e69db1053250fe705c349496
Michael Roberts Jr <mike@sdcs.io>
2019-10-24 15:44:01 -0700

    adds logger and secret for testing

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 let app = express();
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    res.send('Hello Riooooo');
+    let message = process.env.CUSTOM_MSG || "Hello Rio, a Node.js app is running."
+    
+    res.send(`:::: INFO :::: \n ${JSON.stringify(process.env)}\n ::::::: \n ${message}`);
 });
 
 module.exports = app;
