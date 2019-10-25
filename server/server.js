69a6a44c075b5dfe2f955fadfc6b52c80ddf8dfa
Michael Roberts Jr <mike@sdcs.io>
2019-10-24 19:58:57 -0700

    adds a new version and awesome message

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ let app = express();
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {  
-    res.send(`Release ${process.env.APP_VERSION} \n The secret message is: \n ${process.env.CUSTOM_MSG}`);
+    res.send(`This is amazing. Release ${process.env.APP_VERSION} \n The secret message is: \n ${process.env.CUSTOM_MSG}`);
 });
 
 module.exports = app;
